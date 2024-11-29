a7fd62956de914af396303471a1367d63f3a82e8
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-11-29 13:26:57 +0300

    test: refactor tests for reservation

--- reservation/internal/usecase/test/reservation_test.go
+++ reservation/internal/usecase/test/reservation_test.go
@@ -2,16 +2,14 @@ package usecase_test
 
 import (
 	"context"
-	"io"
-	"log/slog"
 	"testing"
 	"time"
 
 	"github.com/SergeyBogomolovv/restaurant/reservation/internal/domain/dto"
+	errs "github.com/SergeyBogomolovv/restaurant/reservation/internal/domain/errors"
 	"github.com/SergeyBogomolovv/restaurant/reservation/internal/usecase"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 func TestReservationUsecase_CreateReservation(t *testing.T) {
@@ -38,32 +36,21 @@ func TestReservationUsecase_CreateReservation(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotEqual(t, reservationID, uuid.Nil)
 	})
-}
 
-func NewTestLogger() *slog.Logger {
-	return slog.New(slog.NewTextHandler(io.Discard, nil))
-}
+	t.Run("table not found", func(t *testing.T) {
+		tableId := uuid.New()
+		dto := &dto.CreateReservationDTO{
+			CustomerID: uuid.New(),
+			TableID:    tableId,
+			StartTime:  time.Unix(1730455200, 0),
+			EndTime:    time.Unix(1730458800, 0),
+		}
 
-type mockReservationRepo struct {
-	mock.Mock
-}
+		mockRepo.On("GetTableExists", ctx, tableId).Return(false, nil)
 
-func (m *mockReservationRepo) CreateReservation(ctx context.Context, dto *dto.CreateReservationDTO) (uuid.UUID, error) {
-	args := m.Called(ctx, dto)
-	return args.Get(0).(uuid.UUID), args.Error(1)
-}
+		id, err := usecase.CreateReservation(ctx, dto)
 
-func (m *mockReservationRepo) SetReservationStatus(ctx context.Context, reservationID uuid.UUID, status string) error {
-	args := m.Called(ctx, reservationID, status)
-	return args.Error(0)
-}
-
-func (m *mockReservationRepo) CloseEndedReservations(ctx context.Context) (int64, error) {
-	args := m.Called(ctx)
-	return args.Get(0).(int64), args.Error(1)
-}
-
-func (m *mockReservationRepo) GetTableExists(ctx context.Context, tableID uuid.UUID) (bool, error) {
-	args := m.Called(ctx, tableID)
-	return args.Get(0).(bool), args.Error(1)
+		assert.Equal(t, id, uuid.Nil)
+		assert.ErrorIs(t, err, errs.ErrTableNotFound)
+	})
 }
